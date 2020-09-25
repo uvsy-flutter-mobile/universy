f9208cab6c3bc316c9f1fe6aea8c7912dc2f371b
loledesma <lorenasoledadledesma@gmail.com>
2020-09-25 01:58:45 -0300

    pending: go back issue and 00:00 issue

--- lib/modules/student/notifications/notification.dart
+++ lib/modules/student/notifications/notification.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:provider/provider.dart';
 import 'package:universy/business/converters/time.dart';
+import 'package:universy/constants/event_types.dart';
 import 'package:universy/constants/notifications.dart';
 import 'package:universy/model/student/event.dart';
 import 'package:universy/modules/student/calendar/calendar.dart';
+import 'package:universy/modules/student/calendar/widget/form/event_type.dart';
 import 'package:universy/services/factory.dart';
+import 'package:universy/text/translators/event_type.dart';
 import 'package:universy/widgets/buttons/raised/rounded.dart';
 import 'package:universy/widgets/buttons/uvsy/cancel.dart';
+import 'package:universy/widgets/cards/rectangular.dart';
 import 'package:universy/widgets/future/future_widget.dart';
 import 'package:universy/widgets/paddings/edge.dart';
 
@@ -15,8 +19,7 @@ class NotificationsAlert extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var sessionFactory = Provider.of<ServiceFactory>(context, listen: false);
-    var studentEventService =
-        sessionFactory.studentEventService(); //TODO: check the dates
+    var studentEventService = sessionFactory.studentEventService();
     return FutureWidget(
       fromFuture:
           studentEventService.getStudentEvents(DateTime.now(), DateTime.now()),
@@ -84,11 +87,14 @@ class NotificationsDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return AlertDialog(
       shape: _buildShapeDialog(),
-      elevation: 80.0,
-      content: _buildBodyDialog(_events),
-      title: Text("Eventos del dia", textAlign: TextAlign.center),
-      contentPadding: EdgeInsets.only(top: 10.0),
-      actions: <Widget>[_buildNavigationToCalendar(context), CancelButton()],
+      content:
+          SizedBox(width: double.maxFinite, child: _buildBodyDialog(context)),
+      title: Text("Tus eventos del dia", textAlign: TextAlign.center),
+      contentPadding: EdgeInsets.only(top: 3.0),
+      actions: <Widget>[
+        _buildNavigationToCalendar(context),
+        CancelButton(onCancel: () => Navigator.of(context).pop())
+      ],
     );
   }
 
@@ -111,20 +117,85 @@ class NotificationsDialog extends StatelessWidget {
     );
   }
 
-  Widget _buildBodyDialog(List<StudentEvent> events) {
+  Widget _buildBodyDialog(BuildContext context) {
     return StaggeredGridView.countBuilder(
       physics: BouncingScrollPhysics(),
+      shrinkWrap: true,
       crossAxisCount: 4,
-      itemCount: events.length,
+      itemCount: _events.length,
       staggeredTileBuilder: (int index) => StaggeredTile.fit(2),
       mainAxisSpacing: 2.0,
       crossAxisSpacing: 1.5,
+      scrollDirection: Axis.vertical,
       itemBuilder: _eventsBuilder,
     );
   }
 
   Widget _eventsBuilder(BuildContext context, int index) {
     StudentEvent event = _events[index];
-    return Text("Hola");
+    return NotificationEvent(event: event);
+  }
+}
+
+class NotificationEvent extends StatelessWidget {
+  final StudentEvent _event;
+
+  const NotificationEvent({Key key, StudentEvent event})
+      : _event = event,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AllEdgePaddedWidget(
+      padding: 8.0,
+      child: Container(
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: Colors.black,
+              width: 2.0,
+            ),
+            color: Colors.grey),
+        child: CircleAvatar(
+          child: _createEventWidget(context),
+          backgroundColor: Colors.white70,
+          radius: 50.0,
+        ),
+      ),
+    );
+  }
+
+  Widget _createEventWidget(BuildContext context) {
+    String timeFrom = _event.timeFrom.format(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          EventType.getIcon(_event.eventType),
+          color: Colors.amber,
+          size: 36.0,
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Center(
+            child: Text(
+          EventTypeDescriptionTranslator().translate(_event.eventType),
+          style: Theme.of(context).primaryTextTheme.caption,
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.clip,
+        )),
+        SizedBox(
+          height: 5,
+        ),
+        Center(
+            child: Text(
+          "$timeFrom",
+          style: Theme.of(context).primaryTextTheme.caption,
+          textAlign: TextAlign.center,
+        )),
+      ],
+    );
   }
 }
