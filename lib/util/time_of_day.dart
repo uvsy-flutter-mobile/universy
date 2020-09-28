0f1055f55468cf731138b752a7a8f67742f0d2b3
loledesma <lorenasoledadledesma@gmail.com>
2020-09-28 20:20:06 -0300

    change int with 00 to 0

--- lib/util/time_of_day.dart
+++ lib/util/time_of_day.dart
@@ -2,16 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:universy/util/object.dart';
 
 class TimeOfDayIntConverter {
-  static const int NULL_INT = 0;
-  static const int DEFAULT_HOUR = 00;
+  static const int DEFAULT_MINUTE = 0;
 
   TimeOfDay convertFromInt(int timeInt) {
-    if (timeInt != NULL_INT) {
+    if (timeInt != DEFAULT_MINUTE) {
       int hours = timeInt ~/ 100;
       int minutes = timeInt % 100;
       return TimeOfDay(hour: hours, minute: minutes);
     }
-    return TimeOfDay(hour: DEFAULT_HOUR, minute: DEFAULT_HOUR);
+    return TimeOfDay(hour: DEFAULT_MINUTE, minute: DEFAULT_MINUTE);
   }
 
   int convertFromTimeOfDay(TimeOfDay time) {
@@ -20,7 +19,7 @@ class TimeOfDayIntConverter {
       int minutes = time.minute;
       return hour * 100 + minutes;
     }
-    return NULL_INT;
+    return DEFAULT_MINUTE;
   }
 }
 
