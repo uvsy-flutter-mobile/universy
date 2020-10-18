dae69ebae33b499e1c7914613ec8eec51e00122f
loledesma <lorenasoledadledesma@gmail.com>
2020-10-17 22:49:03 -0300

    [SCHEDULE]: add cubit, services and display and not found module

--- lib/modules/student/schedule/commons.dart
+++ lib/modules/student/schedule/commons.dart
@@ -9,8 +9,7 @@ class AddScratchButton extends StatelessWidget {
       width: double.infinity,
       child: CustomOutlinedButtonIcon(
         iconData: Icons.add,
-        onPressed: () =>
-            {}, //TODO agregar la accion con la lista de borradores vacia o llena
+        onPressed: () => {},
         labelText: AppText.getInstance().get("student.schedule.addNewSchedule"),
         buttonTextStyle: Theme.of(context).textTheme.button,
         color: Theme.of(context).buttonColor,
